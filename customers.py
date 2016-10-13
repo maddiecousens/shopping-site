6b56755c5bdd3e6d6b4ba3ae26dac0052782e72c
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-13 13:02:58 -0700

    Partway through Customer Class

--- customers.py
+++ customers.py
@@ -4,6 +4,23 @@
 class Customer(object):
     """Ubermelon customer."""
 
-    # TODO: need to implement this
+    def __init__(self, first_name, last_name, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
 
+    def __repr__(self):
+        return "<Customer: %s %s %s" % (self.first_name, self.last_name, self.email)
 
+def read_customers_from_file(filepath):
+    customer_data = {}
+
+    for line in open(filepath):
+        first_name, last_name, email, password = line.strip().split("|")
+
+        customer_data[email] = Customer(first_name, last_name, email, password)
+
+    return customer_data
+
+customers = read_customers_from_file("customers.txt")
